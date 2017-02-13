07a8173aa552f5db4cd33bdcd31ac5276a66f76c
yuenhsi <yuenhsi93@gmail.com>
2017-02-13 13:03:45 -0800

    challenge 1

--- CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -2,6 +2,24 @@
 
 import Foundation
 
-var str = "Hello, playground"
+// challenge 1
+
+func challenge1(input: String) -> Bool {
+    var map = [Character: Int]()
+    for char in input.characters {
+        if map[char] != nil {
+            return false
+        } else {
+            map[char] = 0
+        }
+    }
+    return true
+}
+
+assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
+
 
 //: [Next](@next)
