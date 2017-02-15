83710866444eb48af27f2a9642a5465d435a2482
yuenhsi <yuenhsi93@gmail.com>
2017-02-15 10:53:39 -0800

    up to challenge 5

--- CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -22,4 +22,58 @@ assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
 assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
 
 
+func challenge2(input: String) -> Bool {
+    return input.lowercased().characters.reversed() == Array(input.lowercased().characters)
+}
+
+assert(challenge2(input: "rotator") == true, "Challenge 2 failed")
+assert(challenge2(input: "Rats live on no evil star") == true, "Challenge 2 failed")
+assert(challenge2(input: "Never odd or even") == false, "Challenge 2 failed")
+assert(challenge2(input: "Hello, world") == false, "Challenge 2 failed")
+
+
+func challenge3(first: String, second: String) -> Bool {
+    return Array(first.characters).sorted() == Array(second.characters).sorted()
+}
+
+assert(challenge3(first: "abca", second: "abca") == true, "Challenge 3 failed")
+assert(challenge3(first: "abc", second: "cba") == true, "Challenge 3 failed")
+assert(challenge3(first: " a1 b2 ", second: "b 1 a 2") == true, "Challenge 3 failed")
+assert(challenge3(first: "abc", second: "abca") == false, "Challenge 3 failed")
+assert(challenge3(first: "abc", second: "Abc") == false, "Challenge 3 failed")
+assert(challenge3(first: "abc", second: "cbAa") == false, "Challenge 3 failed")
+
+extension String {
+    func fuzzyContains(_ innerStr: String) -> Bool {
+        return range(of: innerStr, options: .caseInsensitive) != nil
+    }
+}
+
+assert("Hello, world".fuzzyContains("Hello") == true, "Challenge 4 failed")
+assert("Hello, world".fuzzyContains("WORLD") == true, "Challenge 4 failed")
+assert("Hello, world".fuzzyContains("Goodbye") == false, "Challenge 4 failed")
+
+func challenge5(str: String, char: Character) -> Int {
+    
+    var occurences = 0
+    for ch in str.characters {
+        if ch == char {
+            occurences+=1
+        }
+    }
+    return occurences
+}
+
+assert(challenge5(str: "The rain in Spain", char: "a") == 2, "Challenge 5 failed.")
+assert(challenge5(str: "Mississippi", char: "i") == 4, "Challenge 5 failed.")
+assert(challenge5(str: "HackingWithSwift", char: "i") == 3, "Challenge 5 failed.")
+
+
+let names = ["alan","brian","charlie"]
+let csv = names.reduce("===") {text, name in "\(text),\(name)"}
+print(csv)
+// "===,alan,brian,charlie"
+
+
+
 //: [Next](@next)
