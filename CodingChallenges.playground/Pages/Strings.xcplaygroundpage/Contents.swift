c79059ec5908d1ef399296273ea5c93c04fbe556
yuenhsi <yuenhsi93@gmail.com>
2017-02-20 08:40:54 -0800

    do challenges 8-10

--- CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ CodingChallenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -43,6 +43,7 @@ assert(challenge3(first: "abc", second: "abca") == false, "Challenge 3 failed")
 assert(challenge3(first: "abc", second: "Abc") == false, "Challenge 3 failed")
 assert(challenge3(first: "abc", second: "cbAa") == false, "Challenge 3 failed")
 
+
 extension String {
     func fuzzyContains(_ innerStr: String) -> Bool {
         return range(of: innerStr, options: .caseInsensitive) != nil
@@ -52,6 +53,7 @@ extension String {
 assert("Hello, world".fuzzyContains("Hello") == true, "Challenge 4 failed")
 assert("Hello, world".fuzzyContains("WORLD") == true, "Challenge 4 failed")
 assert("Hello, world".fuzzyContains("Goodbye") == false, "Challenge 4 failed")
+
 
 func challenge5(str: String, char: Character) -> Int {
     
@@ -63,6 +65,7 @@ func challenge5(str: String, char: Character) -> Int {
 assert(challenge5(str: "The rain in Spain", char: "a") == 2, "Challenge 5 failed.")
 assert(challenge5(str: "Mississippi", char: "i") == 4, "Challenge 5 failed.")
 assert(challenge5(str: "HackingWithSwift", char: "i") == 3, "Challenge 5 failed.")
+
 
 func challenge6(input: String) -> String {
     var usedChars = [Character]()
@@ -81,6 +84,7 @@ assert(challenge6(input: "wombat") == "wombat", "Challenge 6 failed.")
 assert(challenge6(input: "hello") == "helo", "Challenge 6 failed.")
 assert(challenge6(input: "Mississippi") == "Misp", "Challenge 6 failed.")
 
+
 func challenge7(input: String) -> String {
     return input.replacingOccurrences(of: " +", with: " ", options: .regularExpression)
 }
@@ -88,6 +92,44 @@ func challenge7(input: String) -> String {
 assert(challenge7(input: "a   b  c") == "a b c", "Challenge 7 failed.")
 assert(challenge7(input: "   a") == " a", "Challenge 7 failed.")
 assert(challenge7(input: "abc") == "abc", "Challenge 7 failed.")
+
+
+func challenge8(firstInput: String, secondInput: String) -> Bool {
+    if firstInput.characters.sorted() == secondInput.characters.sorted() {
+        if (firstInput + firstInput).contains(secondInput) {
+            return true
+        }
+    }
+    return false
+}
+
+assert(challenge8(firstInput: "abcde", secondInput: "eabcd") == true, "Challenge 8 failed.")
+assert(challenge8(firstInput: "abcde", secondInput: "cdeab") == true, "Challenge 8 failed.")
+assert(challenge8(firstInput: "abcde", secondInput: "abced") == false, "Challenge 8 failed.")
+assert(challenge8(firstInput: "abc", secondInput: "a") == false, "Challenge 8 failed.")
+
+
+func challenge9(input: String) -> Bool {
+    let set = Set(input.lowercased().characters)
+    let letters = set.filter { $0 >= "a" && $0 <= "z" }
+    return letters.count == 26
+}
+
+assert(challenge9(input: "The quick brown fox jumps over the lazy dog") == true, "Challenge 9 failed.")
+assert(challenge9(input: "The quick brown fox jumped over the lazy dog") == false, "Challenge 9 failed.")
+
+
+func challenge10(input: String) -> (vowels: Int, consonants: Int) {
+    let vowels = [Character](["A","E","I","O","U"])
+    let charactersToCheck = input.uppercased().characters.sorted().filter { $0 >= "A" && $0 <= "Z" }
+    let numberOfVowels = charactersToCheck.filter { vowels.contains($0) }.count
+    let numberOfConsonants = charactersToCheck.count - numberOfVowels
+    return (vowels: numberOfVowels, consonants: numberOfConsonants)
+}
+
+assert(challenge10(input: "Swift Coding Challenges") == (vowels: 6, consonants: 15), "Challenge 10 failed.")
+assert(challenge10(input: "Mississippi") == (vowels: 4, consonants: 7), "Challenge 10 failed.")
+
 
 
 
